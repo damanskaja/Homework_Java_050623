916e3b990428a1566bdc4464dd394e4b32214bb3
damanskaja <129493546+damanskaja@users.noreply.github.com>
2023-06-11 22:56:45 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int limit = 10;
+        int result = 1;
+
+        for (int i = 0; i < limit; i++) {
+            System.out.print(result + " ");
+            result *= 2;
+        }
+    }
+}
